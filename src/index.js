660e8eafce49842ae807ba49df21b801098948cd
Sanyu <sa.los@mail.ru>
2018-09-29 16:00:56 +0300

    fataliti X

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        arr.push(str[i]);
+    }
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    for (let i = arr.length - 1; i >= 0; i--) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (arr[i] == bracketsConfig[j][0] && arr[i + 1] == bracketsConfig[j][1]) {
+                    arr.splice(i, 2);
+                }
+            }
+    }
+    return (arr.length == 0);
 }
+
